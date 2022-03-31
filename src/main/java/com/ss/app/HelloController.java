dc2500afcd5aaf87712dd8ffb9dc79009a2c5e04
susanta <susanta@rbtsb.com>
2022-03-31 21:19:03 +0530

    Tetsing commit via intellij

--- src/main/java/com/ss/app/HelloController.java
+++ src/main/java/com/ss/app/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    // This is a test controller
     @RequestMapping("/")
     public String sayHello(){
         return ("Hello, This is Susanta Testing Spring boot in WildFly");
---
+++ src/main/java/com/ss/app/TestClass.java
@@ -0,0 +1,5 @@
+package com.ss.app;
+
+public class TestClass {
+    // Thhis is a test calss for Intellija testing
+}
